0ba45b484311b2226f3e0cb97f0ce29a8a16877c
marcus-biddle <marcusbiddle16@gmail.com>
2023-09-28 10:43:22 -0700

    initial commit

---
+++ portfolio/src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-logo">
+          Mars.io
+        </div>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
