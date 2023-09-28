e0375561d112f44ae755ec5cd8d4ffbfc88ded71
marcus-biddle <marcusbiddle16@gmail.com>
2023-09-28 11:04:06 -0700

    nav and home comp

---
+++ portfolio/src/components/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section id='home' className='home'>
+        <div className='container'>
+            <p>Engineer | Caffeine Addict</p>
+            <h1>Marcus 'Mars' Biddle</h1>
+            {/* Social Media Container */}
+            <div>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home
