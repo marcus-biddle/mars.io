16af08964d8d268381ac5a3c784f2cb809c6f95b
Mars <marcusbiddle16@gmail.com>
2023-10-03 18:10:00 -0700

    .

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -5,13 +5,13 @@ const Home = () => {
   return (
     <section id='home' className='home'>
         <div className='container'>
-          <div className='desc'>
+          
+            <h1 className=''>Marcus <span>'Mars'</span> Biddle</h1>
+            <div className='desc'>
           <p>QA Engineer</p>
           <p className='divider'>|</p>
           <p>Web Developer</p>
           </div>
-            
-            <h1 className=''>Marcus <span>'Mars'</span> Biddle</h1>
             {/* Social Media Container */}
             <div>
 
